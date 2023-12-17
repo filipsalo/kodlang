a564260f6a507319cba152ed0a938aa972ca3e3f
Filip Salomonsson <filip.salomonsson@gmail.com>
2023-12-17 01:17:11 +0100

    Lexer: Refactor error handling

--- kod/lexer.py
+++ kod/lexer.py
@@ -41,6 +41,24 @@ class Lexer:
             return self.source[self.pos]
         return None
 
+    def error(self, msg):
+        """Raise a syntax error."""
+        err = KodSyntaxError(msg)
+        line_number = self.source.count("\n", 0, self.pos) + 1
+        line_start = self.source.rfind("\n", 0, self.pos) + 1
+        column_number = self.pos - line_start + 1
+        err.line = line_number
+        err.col = column_number
+        lines = self.source.split("\n")
+        for n, line in enumerate(
+            lines[max(0, line_number - 3) : line_number + 3],
+            max(0, line_number - 2),
+        ):
+            err.excerpt += f"{n:3d}: {line}\n"
+            if n == line_number:
+                err.excerpt += f"     {' ' * (column_number - 1)}^\n"
+        return err
+
     def skip_whitespace(self):
         """Skip whitespace and tabs."""
         while self.peek() in (" ", "\t"):
@@ -54,7 +72,7 @@ class Lexer:
     def consume(self, char):
         """Consume the next character, or raise ValueError if it doesn't match"""
         if self.peek() != char:
-            raise ValueError(f"Expected {char}, got {self.peek()!r}")
+            raise self.error(f"Expected {char}, got {self.peek()!r}")
         self.pos += 1
 
     def buffered(self):
@@ -66,8 +84,8 @@ class Lexer:
         value = self.source[self.start : self.pos]
         position = Position(
             self.filename,
-            self.source.count("\n", 0, self.start) + 1,
-            self.pos - self.start + 1,
+            self.start,
+            self.pos,
         )
         self.start = self.pos
         return token_type(value, position)
@@ -95,7 +113,7 @@ class Lexer:
     def lex_identifier(self):
         """Lex an identifier."""
         if not (self.peek().isalpha() or self.peek() == "_"):
-            raise KodSyntaxError(f"Lexer expected identifier, got {self.peek()!r}")
+            raise self.error(f"Lexer expected identifier, got {self.peek()!r}")
         while self.peek().isalnum() or self.peek() == "_":
             self.pos += 1
         match self.buffered():
@@ -131,54 +149,37 @@ class Lexer:
         return list(self)
 
     def __iter__(self):
-        try:
-            while True:
-                self.skip_whitespace()
-                match self.peek():
-                    case None:
-                        yield self.build(EOF)
-                        return
-                    case "\n":
-                        yield self.lex_single_char(EOL)
-                    case "(":
-                        yield self.lex_single_char(OpenParen)
-                    case ")":
-                        yield self.lex_single_char(CloseParen)
-                    case "{":
-                        yield self.lex_single_char(OpenCurly)
-                    case "}":
-                        yield self.lex_single_char(CloseCurly)
-                    case ".":
-                        yield self.lex_single_char(Dot)
-                    case ":":
-                        yield self.lex_single_char(Colon)
-                    case ",":
-                        yield self.lex_single_char(Comma)
-                    case "=":
-                        yield self.lex_single_char(Equals)
-                    case "-":
-                        yield self.lex_arrow()
-                    case '"':
-                        yield self.lex_string()
-                    case ("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"):
-                        yield self.lex_number()
-                    case "/":
-                        yield self.lex_comment()
-                    case _:
-                        yield self.lex_identifier()
-        except KodSyntaxError as e:
-            line_number = self.source.count("\n", 0, self.pos) + 1
-            line_start = self.source.rfind("\n", 0, self.pos) + 1
-            column_number = self.pos - line_start + 1
-            e.line = line_number
-            e.col = column_number
-            lines = self.source.split("\n")
-            for n, line in enumerate(
-                lines[max(0, line_number - 3) : line_number + 3],
-                max(0, line_number - 2),
-            ):
-                e.excerpt += f"{n:3d}: {line}\n"
-                if n == line_number:
-                    e.excerpt += f"     {' ' * (column_number - 1)}^\n"
-
-            raise
+        while True:
+            self.skip_whitespace()
+            match self.peek():
+                case None:
+                    yield self.build(EOF)
+                    return
+                case "\n":
+                    yield self.lex_single_char(EOL)
+                case "(":
+                    yield self.lex_single_char(OpenParen)
+                case ")":
+                    yield self.lex_single_char(CloseParen)
+                case "{":
+                    yield self.lex_single_char(OpenCurly)
+                case "}":
+                    yield self.lex_single_char(CloseCurly)
+                case ".":
+                    yield self.lex_single_char(Dot)
+                case ":":
+                    yield self.lex_single_char(Colon)
+                case ",":
+                    yield self.lex_single_char(Comma)
+                case "=":
+                    yield self.lex_single_char(Equals)
+                case "-":
+                    yield self.lex_arrow()
+                case '"':
+                    yield self.lex_string()
+                case ("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"):
+                    yield self.lex_number()
+                case "/":
+                    yield self.lex_comment()
+                case _:
+                    yield self.lex_identifier()
