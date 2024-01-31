53361b10cdc799ccb6b01d5cc35c2e04693bf66e
Filip Salomonsson <filip.salomonsson@gmail.com>
2024-01-31 23:33:04 +0100

    Shove a debug printing functions into builtins

---
+++ kod/__init__.py
@@ -0,0 +1,14 @@
+import builtins
+import sys
+
+
+def debug_print(*things):
+    """Print to stderr."""
+    print("\033[94m", file=sys.stderr, end="")
+    try:
+        print(*things, file=sys.stderr, end="")
+    finally:
+        print("\033[00m", file=sys.stderr)
+
+
+builtins.dbg = debug_print
